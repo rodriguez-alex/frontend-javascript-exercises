f4d3fa19613068440462dfecf768b560c0147593
Alex Rodriguez <rodriguez-alex@live.com>
2014-08-29 00:57:08 -0500

    passing mocha tests for the boolean assignment

--- 05-booleans/00-basics/challenge.js
+++ 05-booleans/00-basics/challenge.js
@@ -1,19 +1,35 @@
-module.exports.equalStrings = function(stringOne, stringTwo) {
+module.exports.inBetween = function(lower, middle, upper) {
+  return (lower < middle) && (middle < upper);
+};
 
+module.exports.equalStrings = function(stringOne, stringTwo) {
+  return stringOne === stringTwo;
 };
 
 module.exports.notEqual = function(one, two) {
-
-};
-
-module.exports.inBetween = function(lower, middle, upper) {
-
+  return one !== two;
 };
 
 module.exports.outsideRanges = function(number) {
-
+  if(number >= 10 && number <= 20){
+    return false;
+  }
+  else if(number >= 43 && number <= 75){
+    return false;
+  }
+  else if(number >=2 && number <=5){
+    return false;
+  }
+  else {
+    return true;
+  }
 };
 
 module.exports.nameAndPrice = function(name, price) {
-
+  if((name === 'NYTimes' || name === 'LATimes') && price >= 1){
+    return true;
+  }
+  else {
+    return false;
+  }
 };
