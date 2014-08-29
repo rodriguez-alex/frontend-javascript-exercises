85aed558ca806f21cef6b28368506c31c317b881
Alex Rodriguez <rodriguez-alex@live.com>
2014-08-28 23:59:08 -0500

    completed the challenge and questions attached to see if my functions were well written

---
+++ 04-strings/00-basics/_questions.md
@@ -0,0 +1,8 @@
+# Questions for Mentor:
+Why did we make two remote origins?
+
+
+
+# Assignment 04-strings / 00-basics:
+1. Is my **sliceItAndCombineIt** function well written?
+2. Is my substringBetweenMatches function well written?
--- 04-strings/00-basics/challenge.js
+++ 04-strings/00-basics/challenge.js
@@ -1,19 +1,29 @@
 module.exports.formLetter = function(firstName, senderName, message) {
-
+  return "Hello " + firstName + ",\n\n" + message + "\n\nSincerely,\n" + senderName;
 };
 
+//I feel like this can improve
 module.exports.sliceItAndCombineIt = function(bigString, startA, endA, startB, endB) {
 
+  var firstWord = bigString.substring(startA, endA);
+  var secondWord = bigString.substring(startB, endB);
+
+  return firstWord.concat(secondWord);
 };
 
 module.exports.findFirstMatch = function(text, searchString) {
-
+  return text.indexOf(searchString);
 };
 
 module.exports.findLastMatch = function(text, searchString) {
-
+  return text.lastIndexOf(searchString);
 };
 
 module.exports.substringBetweenMatches = function(text, searchString) {
+  var searchStringLength = searchString.length;
+  var posFound = text.indexOf(searchString);
+  var afterPosFound = posFound + searchStringLength;
+  var lastPosFound = text.lastIndexOf(searchString);
 
+  return text.substring(afterPosFound, lastPosFound);
 };
