ffda761dfbb600cce318967aff3795672c46a149
Alex Rodriguez <rodriguez-alex@live.com>
2014-09-02 06:24:29 -0500

    Passing all object mocha tests, last function seems like I did it wrong

--- 09-objects/00-basics/challenge.js
+++ 09-objects/00-basics/challenge.js
@@ -1,7 +1,34 @@
-module.exports.createCourse = undefined;
+module.exports.createCourse = function(title, duration, students){
+  return {
+    title: title,
+    duration: duration,
+    students: students
+  };
+};
 
-module.exports.addProperty = undefined;
+module.exports.addProperty = function(teacher, prop, student){
+  var teacher = teacher;
 
-module.exports.formLetter = undefined;
+  if(!teacher.hasOwnProperty(prop)){
+    teacher[prop] = student;
+  }
 
-module.exports.canIGet = undefined;
+  return teacher;
+};
+
+module.exports.formLetter = function(obj){
+  return "Hello " + obj.recipient + ",\n\n" + obj.msg + "\n\nSincerely,\n" + obj.sender;;
+};
+
+module.exports.canIGet = function(product, moneyOnHand){
+
+  if((product === "Apple Sticker" && moneyOnHand >= 1) ||
+    (product === "MacBook Air" && moneyOnHand >= 999) ||
+    (product === "MacBook Pro" && moneyOnHand >= 1299) ||
+    (product === "Mac Pro" && moneyOnHand >= 2499)){
+    return true;
+  }
+  else {
+    return false;
+  }
+};
